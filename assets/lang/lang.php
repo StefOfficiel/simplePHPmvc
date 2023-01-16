f70790cb5d4afa8cc8eae41f88113fb728bd1e76
StefOfficiel <stephane@s-pichard.fr>
2023-01-16 20:54:34 +0100

    Update lang.php

--- assets/lang/lang.php
+++ assets/lang/lang.php
@@ -1,12 +1,10 @@
 <?php
-require_once('en.php');
-
 if(!empty($_POST['lang']) && $_POST['lang'] != ""){
-	$lang = cq($_POST['lang']);
+	$lang = htmlentities(addslashes($_POST['lang']));
 	setcookie("lang", $lang);
 }
 elseif(!empty($_COOKIE["lang"]) && $_COOKIE["lang"] != ""){
-	$lang = cq($_COOKIE["lang"]);
+	$lang = htmlentities(addslashes($_COOKIE["lang"]));
 }
 else{
 	$lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
